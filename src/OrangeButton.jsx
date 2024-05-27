5b8b99b895d9e063e71eaccec3ab360d62a7d149
Nika M. <merabishvilinikusha@gmail.com>
2024-05-27 21:39:26 +0400

    Add button hover

--- src/OrangeButton.jsx
+++ src/OrangeButton.jsx
@@ -1,6 +1,6 @@
 function OrangeButton({ btnText }) {
   return (
-    <button className=" mb-2 h-11   w-full rounded-3xl bg-primaryOrange text-white">
+    <button className=" mb-2 h-11   w-full rounded-3xl bg-primaryOrange text-white hover:bg-slate-100 hover:text-primaryOrange">
       {btnText}
     </button>
   );
