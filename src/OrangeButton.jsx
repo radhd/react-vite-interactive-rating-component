fb83ec9f95f677dd9a11001498db7e5b65d6a67b
Nika M. <merabishvilinikusha@gmail.com>
2024-05-27 14:39:57 +0400

    Add margin to button

--- src/OrangeButton.jsx
+++ src/OrangeButton.jsx
@@ -1,6 +1,6 @@
 function OrangeButton({ btnText }) {
   return (
-    <button className=" h-11 w-full   rounded-3xl bg-primaryOrange text-white">
+    <button className=" mb-2 h-11   w-full rounded-3xl bg-primaryOrange text-white">
       {btnText}
     </button>
   );
